14c48a9e24f492bd0195bddc1ea7974b97c98083
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2016-08-20 22:53:17 +0300

    Update and rename .gitignore to blink_strobo_1.ino

---
+++ blink_strobo_1.ino
@@ -0,0 +1,32 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  This example code is in the public domain.
+
+change sketch by Nicu FLORICA (aka niq_ro) from http://www.tehnic.go.ro
+http://nicuflorica.blogspot.ro/
+http://arduinotehniq.blogspot.com/
+& http://www.arduinotehniq.com/
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a 1/10 from a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);               // wait for a 1/10 from a second
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a 1/10 from second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);               // wait for 2 second
+  }
